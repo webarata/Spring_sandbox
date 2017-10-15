efee65b411bd0b50dd697c20648a87e969520e49
webarata3 <webarata3@gmail.com>
2017-10-15 19:54:59 +0900

    ファイル名の変更

--- MyBatisSpring01/src/main/java/com/example/mybatis/Main.java
+++ MyBatisSpring01/src/main/java/com/example/mybatis/Main.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:/META-INF/applicationContext.xml");
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:/META-INF/application-context.xml");
         BookDao bookDao = ctx.getBean(BookDao.class);
 
         System.out.println(bookDao.selectBook(101));
